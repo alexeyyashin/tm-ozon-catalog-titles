ca562b968ae06d7a89cc63e155c01ba5fbbc985f
alexeyyashin <47781271+alexeyyashin@users.noreply.github.com>
2023-01-31 16:29:32 +0300

    Script itself

---
+++ script.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Ozon catalog titles
+// @namespace    https://greasyfork.org/en/scripts/459192-ozon-catalog-titles
+// @version      1.1
+// @description  Adds "title" attribute to ozon.ru catalog titles
+// @author       Alexey Yashin <me@alexey-yashin.ru>
+// @match        https://ozon.ru/*
+// @match        https://*.ozon.ru/*
+// @match        http://ozon.ru/*
+// @match        http://*.ozon.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ozon.ru
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(function() {
+        let titles = document.querySelectorAll('.tile-hover-target span span:not([title*=" "]), .tsBodyL span:not([title*=" "])');
+        for (let span of titles) {
+            span.setAttribute('title', span.innerText);
+        }
+    }, 100);
+})();
